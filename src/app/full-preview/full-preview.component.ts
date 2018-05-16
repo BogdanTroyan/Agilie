582171526ab0865b89465503cc6b7b582a72b30a
BogdanTroyan <qzqfaiv@gmail.com>
2018-05-16 16:02:53 +0300

    Update full-preview.component.ts

--- src/app/full-preview/full-preview.component.ts
+++ src/app/full-preview/full-preview.component.ts
@@ -10,19 +10,6 @@ import { ShotService} from '../shot.service';
 })
 export class FullPreviewComponent implements OnInit {
 
-  shotId: any;	
-	shot: any;
-
-  	constructor(private route: ActivatedRoute, private shotService: ShotService) { }
-
-	ngOnInit() {
-	  	this.route.params.subscribe (res => {
-	      	this.shotId = this.route.snapshot.paramMap.get('shotId');
-	    });
-
-	    this.shotService.getShots().subscribe(res => {
-       		this.shot = res[this.shotId];
-     	});
-	}
+  
 
 }
